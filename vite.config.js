c7c46787921bb1938b246368aa3bd0cfba9c6815
Dima Danyliuk <dimadanylyuk@gmail.com>
2024-10-27 00:57:23 +0300

    Fix config vite for tauri

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,12 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
 	plugins: [],
-
-	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-	//
-	// 1. prevent vite from obscuring rust errors
 	clearScreen: false,
-	// 2. tauri expects a fixed port, fail if that port is not available
+	root: "./src",
 	server: {
 		port: 1420,
 		strictPort: true,
 		watch: {
-			// 3. tell vite to ignore watching `src-tauri`
 			ignored: ["**/src-tauri/**"],
 		},
 		open: true,
