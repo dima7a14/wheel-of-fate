95276671c5a97ce942acb7f9060fbf10c23c7982
Dima Danyliuk <dimadanylyuk@gmail.com>
2024-10-27 17:42:24 +0200

    Fix tauri dev config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,30 @@
 import path from "path";
 import { defineConfig } from "vite";
 
+const host = process.env.TAURI_DEV_HOST;
+
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
 	plugins: [],
 	clearScreen: false,
 	root: "./src",
 	server: {
-		port: 1420,
+		host: host || false,
+		port: 5173,
 		strictPort: true,
 		watch: {
 			ignored: ["**/src-tauri/**"],
 		},
 		open: true,
+	},
+	envPrefix: ["VITE_", "TAURI_ENV_*"],
+	build: {
+		target:
+			process.env.TAURI_ENV_PLATFORM == "windows"
+				? "chrome105"
+				: "safari13",
+		minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+		sourcemap: !!process.env.TAURI_ENV_DEBUG,
 	},
 	resolve: {
 		alias: {
